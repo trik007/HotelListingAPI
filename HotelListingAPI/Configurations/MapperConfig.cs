c36aac69633d4fa3202c87eddac14b416c169e10
trik007 <russellkirt@gmail.com>
2024-01-29 12:35:18 +0800

    Added Hotels Endpoint

--- HotelListingAPI/Configurations/MapperConfig.cs
+++ HotelListingAPI/Configurations/MapperConfig.cs
@@ -16,6 +16,8 @@ namespace HotelListingAPI.Configurations
             CreateMap<Country, UpdateCountryDto>().ReverseMap();
 
             CreateMap<Hotel, HotelDto>().ReverseMap();
+            CreateMap<Hotel, CreateHotelDto>().ReverseMap();
+            CreateMap<Hotel, UpdateHotelDto>().ReverseMap();
         }
     }
 }
---
+++ HotelListingAPI/Repository/HotelsRepository.cs
@@ -0,0 +1,15 @@
+﻿using HotelListingAPI.Contracts;
+using HotelListingAPI.Data;
+
+namespace HotelListingAPI.Repository
+{
+    public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
+    {
+        private readonly HotelListingDbContext _context;
+
+        public HotelsRepository(HotelListingDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
